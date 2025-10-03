aface51019a8ff6a3b9cd14b898f4a764527abc4
Anurag Pathak <anurag.pathak8965@gmail.com>
2025-10-03 08:55:51 +0530

    your message

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Button from './component/Button'
+import Header from './component/Header'
+import Hero from './component/Hero'
+import Benefits from './component/Benefits'
+import Collaboration from './component/Collaboration'
+import Services from './component/Services'
+import Pricing from './component/Pricing'
+import Roadmap from './component/Roadmap'
+import Footer from './component/Footer'
+
+const App = () => {
+  return (
+    <>
+     
+    <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden '>
+     <Header/>
+     <Hero/>
+     <Benefits/>
+     <Collaboration/>
+     <Services/>
+     <Pricing/>
+     <Roadmap/>
+     <Footer/>
+    </div>
+    <ButtonGradient/>
+    </>
+  )
+}
+
+export default App
